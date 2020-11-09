4459368f2fda07da5a0c29d308b8f2a7a8c456fe
Ric Taglietti <rictaglietti@gmail.com>
2020-11-09 17:51:44 -0500

    Exercises_1.7

--- projects/part_1/unicafe/src/index.js
+++ projects/part_1/unicafe/src/index.js
@@ -6,6 +6,9 @@ const App = () => {
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
 
+  let average = (good*1 + neutral*0 + bad*-1) / (good + neutral + bad)
+  let positive = (good / (good + neutral + bad))*100
+
   const handleGoodClick = () => {
     setGood(good + 1)
   }
@@ -30,6 +33,13 @@ const App = () => {
         <div>good {good}</div>
         <div>neutral {neutral}</div>
         <div>bad {bad}</div>
+        <div>all {good + neutral + bad}</div>
+        {isNaN(average)
+          ? <div>average: no votes yet!</div>
+          : <div>average: {average.toFixed(2)}</div>}
+        {isNaN(positive)
+          ? <div>positive: no votes yet!</div>
+          : <div>positive: {positive.toFixed(2)}%</div>}
       </div>
     </div>
   )
