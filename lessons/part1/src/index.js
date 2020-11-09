320bc7dc6dabffb73b3a87211d15c329daa0e2bb
Ric Taglietti <rictaglietti@gmail.com>
2020-11-09 15:13:00 -0500

    Part_1c

--- lessons/part1/src/index.js
+++ lessons/part1/src/index.js
@@ -1,17 +1,74 @@
-import React from 'react';
+import React, {useState} from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+
+// const Hello = ({name, age}) => {
+//   const getBirthYear = () => {
+//     return new Date().getFullYear() - age
+//   }
+//   return (
+//     <div>
+//       <p>
+//         Hello {name}, you are {age} years old
+//       </p>
+//       <p>You were probably born in {getBirthYear()}</p>
+//     </div>
+//   )
+// }
+
+// const App = () => {
+//   const name = 'Peter'
+//   const age = 10
+
+//   return (
+//     <div>
+//       <h1>Greetings</h1>
+//       <Hello name="Maya" age={26 + 10} />
+//       <Hello name={name} age={age} />
+//     </div>
+//   )
+
+
+const Display = ({counter}) => {
+  return (
+    <div>
+      {counter}
+    </div>
+  )
+}
+
+const Button = ({handleClick, text}) => {
+  return (
+    <button onClick={handleClick}>{text}</button>
+  )
+}
+
+const App = () => {
+  const [counter, setCounter] = useState(0)
+
+  const increaseByOne = () => {
+    setCounter(counter + 1)
+  }
+
+  const decreaseByOne = () => {
+    setCounter(counter - 1)
+  } 
+
+  const zeroCounter = () => {
+    setCounter(0)
+  }
+
+  return (
+    <div>
+      <Display counter={counter} />
+      <Button handleClick={increaseByOne} text={"increase"} />
+      <Button handleClick={zeroCounter} text={"zero"} />
+      <Button handleClick={decreaseByOne} text={"decrement"} />
+    </div>
+  )
+}
+
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <App/>,
   document.getElementById('root')
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
