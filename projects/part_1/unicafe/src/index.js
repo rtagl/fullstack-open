cc1361b41388e7dc129e93dec0a96ec27e1f670b
Ric Taglietti <rictaglietti@gmail.com>
2020-11-09 19:47:57 -0500

    Exercise_1.8

--- projects/part_1/unicafe/src/index.js
+++ projects/part_1/unicafe/src/index.js
@@ -1,6 +1,24 @@
 import React, {useState} from 'react';
 import ReactDOM from 'react-dom';
 
+const Statistics = ({good, neutral, bad, average, positive}) => {
+  return (
+    <div>
+      <h1>statistics</h1>
+      <div>good {good}</div>
+      <div>neutral {neutral}</div>
+      <div>bad {bad}</div>
+      <div>all {good + neutral + bad}</div>
+      {isNaN(average)
+        ? <div>average: no votes yet!</div>
+        : <div>average: {average.toFixed(2)}</div>}
+      {isNaN(positive)
+        ? <div>positive: no votes yet!</div>
+        : <div>positive: {positive.toFixed(2)}%</div>}
+    </div>
+  )
+}
+
 const App = () => {
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
@@ -29,17 +47,13 @@ const App = () => {
         <button onClick={handleBadClick}>bad</button>
       </div>
       <div>
-        <h1>statistics</h1>
-        <div>good {good}</div>
-        <div>neutral {neutral}</div>
-        <div>bad {bad}</div>
-        <div>all {good + neutral + bad}</div>
-        {isNaN(average)
-          ? <div>average: no votes yet!</div>
-          : <div>average: {average.toFixed(2)}</div>}
-        {isNaN(positive)
-          ? <div>positive: no votes yet!</div>
-          : <div>positive: {positive.toFixed(2)}%</div>}
+        <Statistics 
+          good={good}
+          neutral={neutral}
+          bad={bad}
+          average={average}
+          positive={positive}
+        />
       </div>
     </div>
   )
