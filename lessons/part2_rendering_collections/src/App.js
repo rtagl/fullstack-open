c7a3d1e40ad3ab247a28a927fbb613aefa1d423a
Ric Taglietti <rictaglietti@gmail.com>
2020-11-13 13:31:05 -0500

    Part_2c

--- lessons/part2_rendering_collections/src/App.js
+++ lessons/part2_rendering_collections/src/App.js
@@ -1,10 +1,20 @@
-import React, {useState} from 'react'
+import React, {useState, useEffect} from 'react'
+import axios from 'axios'
 import Note from './components/Note'
 
-const App = (props) => {
-  const [notes, setNotes] = useState(props.notes)
+const App = () => {
+  const [notes, setNotes] = useState([])
   const [newNote, setNewNote] = useState('')
   const [showAll, setShowAll] = useState(true)
+
+
+  useEffect(() => {
+    axios
+      .get('http://localhost:3001/notes')
+      .then(response => {
+        setNotes(response.data)
+    })
+  }, [])
 
   const handleSubmit = (e) => {
     e.preventDefault()
