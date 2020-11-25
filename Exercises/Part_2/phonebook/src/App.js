60b97b90b7a7f20ac20998abdcd01921c35bea29
Ric Taglietti <rictaglietti@gmail.com>
2020-11-25 12:24:46 -0500

    Exercise_2.15 post new person to db

--- Exercises/Part_2/phonebook/src/App.js
+++ Exercises/Part_2/phonebook/src/App.js
@@ -1,61 +1,65 @@
-import React, { useState, useEffect } from 'react'
-import axios from 'axios'
-import Filter from './components/Filter'
-import NewPersonForm from './components/NewPersonForm'
-import PersonsList from './components/PersonsList'
+import React, { useState, useEffect } from "react";
+import personService from "./services/persons";
+import Filter from "./components/Filter";
+import NewPersonForm from "./components/NewPersonForm";
+import PersonsList from "./components/PersonsList";
 
 const App = () => {
-  const [ persons, setPersons ] = useState([]) 
-  const [ newName, setNewName ] = useState('')
-  const [ newNumber, setNewNumber] = useState('')
-  const [ nameFilter, setNameFilter] = useState('')
+  const [persons, setPersons] = useState([]);
+  const [newName, setNewName] = useState("");
+  const [newNumber, setNewNumber] = useState("");
+  const [nameFilter, setNameFilter] = useState("");
 
   useEffect(() => {
-    axios
-      .get('http://localhost:3001/persons')
-      .then(response => {
-        setPersons(response.data)
-      })
-  }, [])
+    personService.getAll().then((initialPersons) => setPersons(initialPersons));
+  }, []);
 
   const handleNewName = (e) => {
-    setNewName(e.target.value)
-  }
+    setNewName(e.target.value);
+  };
 
   const handleNewNumber = (e) => {
-    setNewNumber(e.target.value)
-  }
+    setNewNumber(e.target.value);
+  };
 
   const handleNewPersonSubmit = (e) => {
-    e.preventDefault()
+    e.preventDefault();
 
-    if (persons.some(person => person.name.toLowerCase() === newName.toLowerCase())) {
-      alert(`${newName} is already in phonebook`)
-      setNewName('')
+    if (
+      persons.some(
+        (person) => person.name.toLowerCase() === newName.toLowerCase()
+      )
+    ) {
+      alert(`${newName} is already in phonebook`);
+      setNewName("");
     } else {
       const person = {
         name: newName,
-        number: newNumber
-      }
-      setPersons(persons.concat(person))
-      setNewName('')
-      setNewNumber('')
+        number: newNumber,
+      };
+      personService.create(person).then((returnedPerson) => {
+        setPersons(persons.concat(returnedPerson));
+        setNewName("");
+        setNewNumber("");
+      });
     }
-  }
+  };
 
   const handleFilter = (e) => {
-    setNameFilter(e.target.value)
-  }
+    setNameFilter(e.target.value);
+  };
 
-  const personsToShow = persons.filter(person => person.name.toLowerCase().includes(nameFilter.toLowerCase()))
+  const personsToShow = persons.filter((person) =>
+    person.name.toLowerCase().includes(nameFilter.toLowerCase())
+  );
 
   return (
     <div>
       <h2>Phonebook</h2>
-      <Filter nameFilter={nameFilter} handleFilter={handleFilter}/>
+      <Filter nameFilter={nameFilter} handleFilter={handleFilter} />
       <div>
         <h2>Add a new</h2>
-        <NewPersonForm 
+        <NewPersonForm
           newName={newName}
           newNumber={newNumber}
           handleNewName={handleNewName}
@@ -66,7 +70,7 @@ const App = () => {
       <h2>Numbers</h2>
       <PersonsList personsToShow={personsToShow} />
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
