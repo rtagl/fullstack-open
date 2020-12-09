57623107603a16ee7d718fcb8e3b4bb3ebaba6c0
Ric Taglietti <rictaglietti@gmail.com>
2020-12-09 16:58:36 -0500

    Exercise_3.8 create  morgan logger token

--- Exercises/Part_3/phonebook_server/index.js
+++ Exercises/Part_3/phonebook_server/index.js
@@ -4,7 +4,15 @@ const morgan = require("morgan");
 
 app.use(express.json());
 
-app.use(morgan("tiny"));
+morgan.token("request-body", function (req, res) {
+  return JSON.stringify(req.body);
+});
+
+app.use(
+  morgan(
+    ":method :url :status :res[content-length] - :response-time :request-body"
+  )
+);
 
 // const requestLogger = (request, response, next) => {
 //   console.log("Method: ", request.method);
