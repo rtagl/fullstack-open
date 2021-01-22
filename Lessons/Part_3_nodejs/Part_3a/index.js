bc2d993d90304c4f16101972f4c605b03b3cfef0
Ric Taglietti <rictaglietti@gmail.com>
2021-01-22 11:05:33 -0500

    post and get notes to database

--- Lessons/Part_3_nodejs/Part_3a/index.js
+++ Lessons/Part_3_nodejs/Part_3a/index.js
@@ -1,5 +1,7 @@
+require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
+const Note = require("./models/note");
 const app = express();
 
 app.use(cors());
@@ -15,44 +17,20 @@ const requestLogger = (request, response, next) => {
 
 app.use(requestLogger);
 
-let notes = [
-  {
-    id: 1,
-    content: "HTML is easy",
-    date: "2019-05-30T17:30:31.098Z",
-    important: true,
-  },
-  {
-    id: 2,
-    content: "Browser can execute only Javascript",
-    date: "2019-05-30T18:39:34.091Z",
-    important: false,
-  },
-  {
-    id: 3,
-    content: "GET and POST are the most important methods of HTTP protocol",
-    date: "2019-05-30T19:20:14.298Z",
-    important: true,
-  },
-];
-
 app.get("/", (req, res) => {
   res.send("<h1>Hello World</h1>");
 });
 
 app.get("/api/notes", (req, res) => {
-  res.json(notes);
+  Note.find({}).then((notes) => {
+    res.json(notes);
+  });
 });
 
 app.get("/api/notes/:id", (req, res) => {
-  const id = Number(req.params.id);
-  const note = notes.find((note) => note.id === id);
-
-  if (note) {
+  Note.findById(req.params.id).then((note) => {
     res.json(note);
-  } else {
-    res.status(404).end();
-  }
+  });
 });
 
 app.delete("/api/notes/:id", (req, res) => {
@@ -66,16 +44,15 @@ app.post("/api/notes", (req, res) => {
   if (!body.content) {
     return res.status(400).json({ error: "content missing" });
   }
-  const newNote = {
-    id: notes.length + 1,
+  const note = new Note({
     content: body.content,
     date: new Date().toISOString(),
-    important: Math.random < 0.5 ? true : false,
-  };
+    important: body.important || false,
+  });
 
-  notes = notes.concat(newNote);
-
-  res.json(newNote);
+  note.save().then((savedNote) => {
+    res.json(savedNote);
+  });
 });
 
 const unknownEndpoint = (request, response) => {
