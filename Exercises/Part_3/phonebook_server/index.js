3de3eb046f74961cc23210b41fad58c293f86b37
Ric Taglietti <rictaglietti@gmail.com>
2021-01-22 12:13:43 -0500

    get and post people to database

--- Exercises/Part_3/phonebook_server/index.js
+++ Exercises/Part_3/phonebook_server/index.js
@@ -1,5 +1,7 @@
+require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
+const Person = require("./models/person");
 const app = express();
 const morgan = require("morgan");
 
@@ -17,41 +19,16 @@ app.use(
   )
 );
 
-let persons = [
-  {
-    name: "Arto Hellas",
-    number: "94",
-    id: 1,
-  },
-  {
-    name: "Ada Lovelace",
-    number: "293",
-    id: 2,
-  },
-  {
-    name: "Dan Abramov",
-    number: "12-43-234245",
-    id: 3,
-  },
-  {
-    name: "Mary Poppendieck",
-    number: "39-23-6423122",
-    id: 4,
-  },
-];
-
 app.get("/api/persons", (req, res) => {
-  res.json(persons);
+  Person.find({}).then((persons) => {
+    res.json(persons);
+  });
 });
 
 app.get("/api/persons/:id", (req, res) => {
-  const id = Number(req.params.id);
-  const person = persons.find((person) => person.id === id);
-  if (person) {
+  Person.findById(req.params.id).then((person) => {
     res.json(person);
-  } else {
-    res.status(404).end();
-  }
+  });
 });
 
 app.get("/info", (req, res) => {
@@ -76,19 +53,16 @@ app.post("/api/persons", (req, res) => {
 
   if (!body.name || !body.number) {
     return res.status(400).json({ error: "content missing" });
-  } else if (persons.some((person) => person.name === body.name)) {
-    return res.status(400).json({ error: "name must be unique" });
   }
 
-  const newPerson = {
+  const person = new Person({
     name: body.name,
     number: body.number,
-    id: Math.round(Math.random() * 10000),
-  };
+  });
 
-  persons = persons.concat(newPerson);
-
-  res.json(newPerson);
+  person.save().then((savedPerson) => {
+    res.json(savedPerson);
+  });
 });
 
 const unknownEndpoint = (req, res) => {
