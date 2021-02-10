390ab47c102cd9860c00062919dcd5b3cb82d6dd
Ric Taglietti <rictaglietti@gmail.com>
2021-02-10 15:50:21 -0500

    uibuild

--- Lessons/Part_3_nodejs/Part_3a/index.js
+++ Lessons/Part_3_nodejs/Part_3a/index.js
@@ -51,20 +51,22 @@ app.delete("/api/notes/:id", (req, res) => {
     });
 });
 
-app.post("/api/notes", (req, res) => {
+app.post("/api/notes", (req, res, next) => {
   const body = req.body;
-  if (!body.content) {
-    return res.status(400).json({ error: "content missing" });
-  }
+
   const note = new Note({
     content: body.content,
     date: new Date().toISOString(),
     important: body.important || false,
   });
 
-  note.save().then((savedNote) => {
-    res.json(savedNote);
-  });
+  note
+    .save()
+    .then((savedNote) => savedNote.toJSON())
+    .then((savedAndFormattedNote) => {
+      res.json(savedAndFormattedNote);
+    })
+    .catch((error) => next(error));
 });
 
 app.put("/api/notes/:id", (req, res, next) => {
@@ -92,7 +94,10 @@ const errorHandler = (error, req, res, next) => {
   console.log(error.message);
   if (error.name === "CastError") {
     return res.status(400).send({ error: "malformatted id" });
+  } else if (error.name === "ValidationError") {
+    return res.status(400).send({ error: error.message });
   }
+  next(error);
 };
 
 app.use(errorHandler);
