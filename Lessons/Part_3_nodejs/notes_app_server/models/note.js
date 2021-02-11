43a3d42cc5de4498abc7cd84ebc97d26dd503db3
Ric Taglietti <rictaglietti@gmail.com>
2021-02-11 14:19:14 -0500

    uibuild

--- Lessons/Part_3_nodejs/notes_app_server/models/note.js
+++ Lessons/Part_3_nodejs/notes_app_server/models/note.js
@@ -1,28 +1,10 @@
 const mongoose = require("mongoose");
 
-const url = process.env.MONGODB_URI;
-
-console.log("connecting to:", url);
-
-mongoose
-  .connect(url, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-    useFindAndModify: false,
-    useCreateIndex: true,
-  })
-  .then((result) => {
-    console.log("connected to MongoDB");
-  })
-  .catch((error) => {
-    console.log("error connecting to MongoDB:", error.message);
-  });
-
 const noteSchema = new mongoose.Schema({
   content: {
     type: String,
-    minlength: 5,
     required: true,
+    minlength: 5,
   },
   date: {
     type: Date,
