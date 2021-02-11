0e32b91aa80df355c2c883b9689bf39380833f47
Ric Taglietti <rictaglietti@gmail.com>
2021-02-11 13:47:11 -0500

    uibuild

--- Lessons/Part_3_nodejs/notes_app_server/index.js
+++ Lessons/Part_3_nodejs/notes_app_server/index.js
@@ -12,6 +12,7 @@ const requestLogger = (request, response, next) => {
   console.log("Method: ", request.method);
   console.log("Path: ", request.path);
   console.log("Body: ", request.body);
+  console.log("howdy");
   next();
 };
 
